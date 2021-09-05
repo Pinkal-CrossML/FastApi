b72a8a056e0fce7aeef0bbe3561d00f1a3b0f703
pinkal-crossml <pinkal@crossnl.com>
2021-09-05 20:07:45 +0530

    second commit

--- blog/main.py
+++ blog/main.py
@@ -1,16 +1,42 @@
-from fastapi import FastAPI
-from fastapi.params import Body
-# from pydantic import
+from fastapi import FastAPI, Depends, status, Response,HTTPException
+from . import schemas, models
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
+import blog
 
 app = FastAPI()
 
-class Blog():
-      title:str
-      
-      body:
+models.Base.metadata.create_all(engine)
+
+def get_db():
+      db = SessionLocal()
+      try:
+            yield db
+      finally:
+            db.close()
+
 
 @app.post('/blog')
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+      new_blog = models.Blog(title=request.title, body=request.body)
+      db.add(new_blog)
+      db.commit()
+      db.refresh(new_blog)
+      return new_blog
 
-def create(request: Blog):
+@app.get('/blog')
+def all(db: Session = Depends(get_db)):
+      blogs = db.query(models.Blog).all()
+      return blogs
 
-      return request
+
+@app.get('/blog/{id}', status_code=200,response_model=schemas.ShowBlog)
+def show(id, response: Response, db: Session = Depends(get_db)):
+      blog = db.query(models.Blog).filter(models.Blog.id == id).filter()
+      if not blog:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                              detail = f"Blog with the id {id} is not available")
+
+
+      return blog
